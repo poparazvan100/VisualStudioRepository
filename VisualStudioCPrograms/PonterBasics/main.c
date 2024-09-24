acc1e7302167c91eae7051e53e03df51edb09237
Popa <uid94750@contiwan.com>
2024-09-24 12:58:53 +0300

    update PointerBasics/main.c

--- VisualStudioCPrograms/PonterBasics/main.c
+++ VisualStudioCPrograms/PonterBasics/main.c
@@ -9,7 +9,7 @@ void swap(int* a, int* b);
 void squareInteger(int* x);
 
 int main() {
-	/***********************************************/
+	/* main starts here */
 	printf("******* PointerBasics ******\n\n");
 
 	int myVariable = 15;
