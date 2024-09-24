1678a0415bf7f9f77fa161d43eccb7927e9d6b01
Popa <uid94750@contiwan.com>
2024-09-24 13:24:33 +0300

    added Structures project

---
+++ VisualStudioCPrograms/Structures/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	printf("*******  Structures ******\n\n");
+
+	struct date {
+		int month;
+		int day;
+		int year;
+	};
+
+	printf("\n");
+	//system("pause");
+	return 0;
+}
